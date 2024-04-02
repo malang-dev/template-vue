f79af392e25905793c1608100792493d0c63e0c8
Bramanda Suwandi <bramanda48@gmail.com>
2024-04-03 01:58:53 +0700

    fix: error unit tests "_ctx.$t is not a function"

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,15 @@
+import { config, mount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
-
-import { mount } from "@vue/test-utils";
 import HelloWorld from "../HelloWorld.vue";
+
+// References: https://stackoverflow.com/questions/72260793/how-to-mock-stub-vue-i18n#answer-75485837
+config.global.mocks = {
+  $t: (tKey: string) => tKey,
+};
 
 describe("HelloWorld", () => {
   it("renders properly", () => {
-    const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } });
-    expect(wrapper.text()).toContain("Hello Vitest");
+    const wrapper = mount(HelloWorld, { props: { msg: "Hello World!" } });
+    expect(wrapper.text()).toContain("Hello World!");
   });
 });
