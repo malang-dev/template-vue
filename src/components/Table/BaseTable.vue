a3e444b997316e5ef845358c46af8f1cc30c55cc
Bramanda Suwandi <bramanda48@gmail.com>
2024-04-07 23:44:18 +0700

    refactor: remove unused script setup

--- src/components/Table/BaseTable.vue
+++ src/components/Table/BaseTable.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
     <slot></slot>
--- src/components/Table/BaseTableCell.vue
+++ src/components/Table/BaseTableCell.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <td class="px-4 py-4 text-sm whitespace-nowrap">
     <slot></slot>
