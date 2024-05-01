3f02a8c1943eab1c7fabe27963cb72b5aefd055d
Bramanda Suwandi <bramanda48@gmail.com>
2024-05-01 13:39:33 +0700

    fix: invalid logo url in github pages 2

--- src/utils/baseurl.ts
+++ src/utils/baseurl.ts
@@ -1,3 +1,7 @@
 export function baseURL(path: string) {
-  return import.meta.env.BASE_URL + path;
+  return suffix(import.meta.env.BASE_URL, "/") + path.replace(/^\/*/g, "");
+}
+
+function suffix(string: string, suffix: string) {
+  return string.endsWith(suffix) ? string : string + suffix;
 }
