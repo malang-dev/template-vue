5987bcd66a93d1ef115d7401c51a39fafc06b532
Bramanda Suwandi <bramanda48@gmail.com>
2024-06-28 14:35:26 +0700

    feat: remove all base. prepare switch to shadcn

--- src/components/Table/BaseTableCell.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <td class="px-4 py-4 text-sm whitespace-nowrap">
-    <slot></slot>
-  </td>
-</template>
--- src/components/Table/BaseTableHeadCell.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <th
-    scope="col"
-    class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-  >
-    <slot></slot>
-  </th>
-</template>
